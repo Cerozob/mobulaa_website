df9400b6c1bfa919a235dc79c33cfa9c20c4b6fd
Camilo Rozo <ce.rozob@uniandes.edu.co>
2023-05-05 00:16:18 -0500

    'deploying

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://cerozob.github.io/mobulaa_website/sw.js', { scope: 'https://cerozob.github.io/mobulaa_website/' })})}
